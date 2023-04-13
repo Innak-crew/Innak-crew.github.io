a5c590818367f5ac7b62e153e31e974df043989d
Innak-crew <124018200+Innak-crew@users.noreply.github.com>
2023-04-13 15:23:30 +0530

    Delete routes.js

--- lat.api/routes.js
+++
@@ -1,5 +0,0 @@
-module.exports = app => {
-    require("./app/modules/student/students.routes")(app);
-	require("./app/modules/question/questions.routes")(app);
-	require("./app/modules/student_program/student_programs.routes")(app);
-};
